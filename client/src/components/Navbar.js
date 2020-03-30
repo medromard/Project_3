f4d4de60491728445a6a2bc2c3efb1aeee8952bb
Adam Espino <aespino1987@gmail.com>
2020-03-29 23:17:01 -0500

    Removal of uneccesary file and setup of react

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import login, { Login } from './loginModal'
+
+export class Navbar extends Component {
+    componentDidMount(){}
+
+    render() {
+        return (
+            <div>
+            <nav>
+                <div className="nav-wrapper black">
+                    <div className="container">
+                    <a href="#!" className="brand-logo">Logo</a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><Login/></li>
+                       
+                        <li><a className="waves-effect waves-light modal-trigger btn">Register</a></li>
+
+                       
+                    </ul>
+                    </div>
+            </div>
+            </nav>
+
+            </div>
+        )
+    }
+}
+
+export default Navbar
+
---
+++ client/src/components/loginModal.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+    componentDidMount(){
+        const M = window.M;
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.modal');
+            var instances = M.Modal.init(elems, {});
+          });
+    }
+
+    render() {
+        return (
+            <div>
+           
+                <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Login</a>
+                <div id="modal1" class="modal">
+                    <div className="modal-content">
+                        <h4>Modal Header</h4>
+                        <p>A bunch of text</p>
+                    </div>
+                    <div className="modal-footer">
+                         <a href="#!" class="modal-close waves-effect waves-green btn-flat">Login</a>
+                    </div>
+                 </div>
+
+            </div>
+        )
+    }
+}
+
+export default Login
+
