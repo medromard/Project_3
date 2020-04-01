f33e8d2c62952d1a8c58ea6d897634b81f9cf3bf
Adam Espino <aespino1987@gmail.com>
2020-03-31 21:02:59 -0500

    Added cards

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import M from 'materialize-css';
 
+import Item from './Card'
 const Grid = () => {
     return (
         <div className = "container">
@@ -9,8 +10,12 @@ const Grid = () => {
                     <p className = "center-align fs-18" > Our application is designed for the user to see what the hottest deals are for a particular product. In our demo app, it will be designed for the avid shoe collector. This app will allow the user to create a profile, to be able to save and access the site, and search the type of shoe(s) that they are looking for. The site will then populate search results based on the best deals around the web or in the stores. Also this site will give you a notification on when the newest release of whatever product that they are looking for.</p>
                 </div>
             </div>
+
+            <div className = "row">
+                <Item/>
+            </div>
         </div>
     );
 };
 
-export default Grid;
+export default Grid; 
