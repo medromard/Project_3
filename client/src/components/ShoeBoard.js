2ca29dd4b84c670d3de639e40b2018cff68510a2
Adam Espino <aespino1987@gmail.com>
2020-04-02 20:08:46 -0500

    Setup shoeboard to take in data from backend

--- client/src/components/ShoeBoard.js
+++ client/src/components/ShoeBoard.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Row, Col, Card, CardTitle, Icon } from 'react-materialize';
+import React, { useState, useEffect } from 'react';
+import { Row, Col} from 'react-materialize';
 import Item from '../components/Card'
 
 
@@ -9,6 +9,34 @@ function ShoeBoard() {
     const [state, setState] = useState({
         array: [0, 1, 0, 1, 0, 1]
     })
+
+    useEffect(() => {
+        rssScrape()
+    }, [])
+    
+    function rssScrape() {
+        fetch('https://hypebeast.com/footwear/feed')
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+          console.log(data);
+        //   const items = data.querySelectorAll("item");
+        //   let html = ``;
+        //   items.forEach(el => {
+        //     html += `
+        //       <article>
+        //         <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
+        //         <h2>
+        //           <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+        //             ${el.querySelector("title").innerHTML}
+        //           </a>
+        //         </h2>
+        //       </article>
+        //     `;
+        //   });
+        //   document.body.insertAdjacentHTML("beforeend", html);
+        });
+    }
     return (
         <div className="container">
            
