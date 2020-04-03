596da328a5aa1fa7a5230d01f4c9fb55a9042a93
Adam Espino <aespino1987@gmail.com>
2020-04-02 22:14:39 -0500

    Added import data to ShoeBoard

--- client/src/components/ShoeBoard.js
+++ client/src/components/ShoeBoard.js
@@ -1,51 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col} from 'react-materialize';
-import Item from '../components/Card'
-
-
+import { Row, Col, Card, CardTitle, Icon } from 'react-materialize';
+import Item from '../components/Card';
+import API from '../utils/API';
 
 
 function ShoeBoard() {
     const [state, setState] = useState({
-        array: [0, 1, 0, 1, 0, 1]
+        array: [0, 1]
     })
 
     useEffect(() => {
-        rssScrape()
-    }, [])
-    
-    function rssScrape() {
-        fetch('https://hypebeast.com/footwear/feed')
-        .then(response => response.text())
-        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-        .then(data => {
-          console.log(data);
-        //   const items = data.querySelectorAll("item");
-        //   let html = ``;
-        //   items.forEach(el => {
-        //     html += `
-        //       <article>
-        //         <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
-        //         <h2>
-        //           <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
-        //             ${el.querySelector("title").innerHTML}
-        //           </a>
-        //         </h2>
-        //       </article>
-        //     `;
-        //   });
-        //   document.body.insertAdjacentHTML("beforeend", html);
-        });
-    }
+        // code to run on component mount
+        API.getShoes().then(results =>
+            {
+                setState({
+                    array: results.data.items
+                })
+            })
+      }, [])
+
     return (
         <div className="container">
-           
+            {}
             <Row>
 
                 {state.array.map(item => (
                     <Col
                         s={12}
-                        m={6}><Item /></Col>))}
+                        m={6}>
+                         {/* {JSON.stringify(item)} */}
+                            <Item 
+                                title={item.title}
+                                author={item.author}
+                                link={item.link}
+                            />
+                    </Col>))}
             </Row>
 
         </div>
